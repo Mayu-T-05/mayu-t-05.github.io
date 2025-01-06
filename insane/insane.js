97e5fb6b3c471157ea75f2323105c5ba4f4c9edd
Mayu-T-05 <tokimu0523@gmail.com>
2025-01-06 16:30:26 +0900

    Update insane.js

--- insane/insane.js
+++ insane/insane.js
@@ -9,3 +9,19 @@ function copyCP() {
         alert("コピーに失敗しました: " + error);
     });
 }
+
+//恐怖心の二段階プルダウン
+function change(e) {
+    // 親セレクタで選択したカテゴリ番号
+    const selectCategory = e.options[e.selectedIndex].dataset.category;
+    // 子セレクタのoptionリスト
+    const childs = document.getElementById('children').children;
+    for(let i in childs) {
+        const child = childs[i];
+        // 子セレクタのoptionにdata-categoryが設定されている
+        if(child.dataset !== undefined && child.dataset.category !== undefined) {
+            // 親で選択したcategoryと異なればdisabledをtrueに、同じならfalseに変更
+            child.disabled = child.dataset.category !== selectCategory ? true : false;
+        }
+    }
+}
